1521b1c6ad03f50fa991b35e11508afb2befbb95
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-11-11 19:14:42 +0300

    3nd todo

--- calculator.js
+++ calculator.js
@@ -180,3 +180,32 @@ import _ from  "lodash" ;
 
 const operation = process.argv[2]
 const numbers = process.argv.slice(3);
+
+if( isValidOperation(operation) ){
+  let result;
+  const nums = parseNumbers(numbers)
+  switch (operation) {
+  case "add":
+    result = add(nums);
+    break
+  case "subtract":
+    result = subtract(nums);
+    break
+  case "multiply":
+    result = multiply(nums);
+    break
+
+  case "divide":
+    result = divide(nums);
+    break
+
+  }
+
+  console.log(`Result: ${result}`);
+
+}
+
+else {
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide");
+  return;
+}
