1183bf21aeb047356cb1960972478cfd11db209e
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-11-11 19:05:58 +0300

    2nd todo

--- calculator.js
+++ calculator.js
@@ -176,4 +176,7 @@ After completing all TODOs, test your calculator:
 */
 import { add, subtract,multiply , divide } from "./utils/operations.js"
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
-import _ from  lodash ;
+import _ from  "lodash" ;
+
+const operation = process.argv[2]
+const numbers = process.argv.slice(3);
